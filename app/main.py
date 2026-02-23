0bb63398255a0bbc099938f71546a05b1134d40c
KauaVictor077 <kaua38460@gmail.com>
2026-02-23 16:51:22 -0300

    Implement CinemaBar class for selling products
    Added CinemaBar class with sell_product method.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,7 @@
->>> import pkg.mod1, pkg.mod2
->>> pkg.mod1.foo()
-[mod1] foo()
->>> x = pkg.mod2.Bar()
->>> x
-<pkg.mod2.Bar object at 0x033F7290>
->>> from pkg.mod1 import foo
->>> foo()
-[mod1] foo()
->>> from pkg.mod2 import Bar as Qux
->>> x = Qux()
->>> x
-<pkg.mod2.Bar object at 0x036DFFD0>
+from app.people.customer import Customer
+
+
+class CinemaBar:
+    @staticmethod
+    def sell_product(product: str, customer: Customer) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
