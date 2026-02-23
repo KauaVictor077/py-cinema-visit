6c8b8f3a56681bf64fb06887d5e708ea7060c042
KauaVictor077 <kaua38460@gmail.com>
2026-02-23 16:47:05 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-1| import pkg.mod1
-2| import pkg.mod2
-3| from pkg.mod1 import foo
-4| from pkg.mod2 import Bar as Qux
-5| 
-6| bar_instance = pkg.mod2.Bar()
-7| foo()
-8| qux_instance = Qux()
+import pkg.mod1
+import pkg.mod2
+from pkg.mod1 import foo
+from pkg.mod2 import Bar as Qux
+
+bar_instance = pkg.mod2.Bar()
+foo()
+qux_instance = Qux()
