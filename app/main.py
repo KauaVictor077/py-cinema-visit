f0583278838e0925fc1780ee41a7e02ffbc262c3
KauaVictor077 <kaua38460@gmail.com>
2026-02-23 16:45:06 -0300

    Refactor imports and update instance creation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-import pkg.mod1, pkg.mod2
-pkg.mod1.foo()
-x = pkg.mod2.Bar()
-from pkg.mod1 import foo
-foo()
-from pkg.mod2 import Bar as Qux
-x = Qux()
+1| import pkg.mod1
+2| import pkg.mod2
+3| from pkg.mod1 import foo
+4| from pkg.mod2 import Bar as Qux
+5| 
+6| bar_instance = pkg.mod2.Bar()
+7| foo()
+8| qux_instance = Qux()
