755dc03a87de75194f092a778609a0ef997537bc
KauaVictor077 <kaua38460@gmail.com>
2026-02-23 16:41:17 -0300

    Add imports and example usage in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-# write your imports here
-
-
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+>>> import pkg.mod1, pkg.mod2
+>>> pkg.mod1.foo()
+[mod1] foo()
+>>> x = pkg.mod2.Bar()
+>>> x
+<pkg.mod2.Bar object at 0x033F7290>
+>>> from pkg.mod1 import foo
+>>> foo()
+[mod1] foo()
+>>> from pkg.mod2 import Bar as Qux
+>>> x = Qux()
+>>> x
+<pkg.mod2.Bar object at 0x036DFFD0>
